8d742e64f91cdf337cba4654fb13071b4cfdfe16
Aayush Kumar Sahu <aayushmau5@gmail.com>
2024-03-24 13:59:42 +0530

    update elixir and erlang version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Battleship.MixProject do
     [
       app: :battleship,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
