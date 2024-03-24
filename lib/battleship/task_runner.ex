6e9c60c610963a1bf18626749a17f914ab318de5
Aayush Kumar Sahu <aayushmau5@gmail.com>
2024-03-24 21:29:04 +0530

    add remote connection script and remote node task runner

---
+++ lib/battleship/task_runner.ex
@@ -0,0 +1,41 @@
+defmodule Battleship.TaskRunner do
+  require Logger
+
+  @remote_node_name "phoenix-aayushsahu-com"
+
+  def get_task_runner_node() do
+    Node.list()
+    |> Enum.find(nil, fn node_name ->
+      node_name
+      |> Atom.to_string()
+      |> String.contains?(@remote_node_name)
+    end)
+  end
+
+  def run(task_info, remote_node) do
+    %{module: module, function: function, args: args} = task_info
+
+    Task.async(fn ->
+      Process.flag(:trap_exit, true)
+
+      _t2 =
+        Task.Supervisor.async_nolink(
+          # Accumulator.TaskRunner is a task supervisor that's running on remote node
+          {Accumulator.TaskRunner, remote_node},
+          module,
+          function,
+          args
+        )
+
+      receive do
+        {:DOWN, _, _, pid, _reason} ->
+          Logger.error("Task execution failed with PID: #{pid}")
+          nil
+
+        {_ref, result} ->
+          result
+      end
+    end)
+    |> Task.await()
+  end
+end
